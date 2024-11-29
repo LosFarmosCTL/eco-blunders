5180073cd20aa03d6ce10082d7d4c884da883195
BunkeMonkey <135245949+BunkeMonkey@users.noreply.github.com.>
2024-11-29 17:12:03 +0100

    fix: made OSM api typescript acceptable, added event listener to inoput; feat:added wrapper for fetching, added interface for osm data

--- src/location_script.ts
+++ src/location_script.ts
@@ -1,53 +1,37 @@
-import { getOsmData } from "./osm_api.ts"
+import { getOsmData } from './osm_api.ts'
 
 //get all the HTML elems here
-let streetInput = <HTMLInputElement>document.body.querySelector("#street")
-let locationAutocomplete = <HTMLElement>document.body.querySelector('[name="loc_autocomplete"]')
-let loc1text = <HTMLElement>document.body.querySelector('[name="loc1"]')
-
-
-let OSM = {
-    street: "empty",
-    house_number: "empty",
-    zip: "empty",
-    lat: "empty",
-    long: "empty"
-}
+const streetInput = document.body.querySelector<HTMLInputElement>('#street')
+const locationAutocomplete =
+  document.body.querySelector<HTMLElement>('#loc_autocomplete')
+const loc1text = document.body.querySelector<HTMLElement>('#loc1')
 
 let OSMTimeoutID: number
 let streetStringOld: string
 
-streetInput.onkeyup = async () => {
-    let streetString: string = streetInput.value
-    //introduce timeout to this and reduce limit
-    if (streetString == streetStringOld){
-        return;
-    }
-    console.log(streetString)
-    window.clearTimeout(OSMTimeoutID)
-    streetStringOld = streetString
+streetInput?.addEventListener('input', function () {
+  console.log('change')
+  const streetString: string = streetInput.value
+  //introduce timeout to this and reduce limit
+  if (streetString == streetStringOld) {
+    return
+  }
+  console.log(streetString)
+  window.clearTimeout(OSMTimeoutID)
+  streetStringOld = streetString
 
+  if (streetString.length < 5) {
+    locationAutocomplete?.classList.add('hidden')
+    return
+  }
 
-    if (streetString.length < 5) {
-        locationAutocomplete.style["display"] = "none"
-        return 
-    }
+  //store timeout in var
+  //every call do the cleartimeout and reinvoke it
 
-    //store timeout in var 
-    //every call do the cleartimeout and reinvoke it
-    
-    
-    OSMTimeoutID = window.setTimeout( async () => {
-        OSM = await getOsmData(streetString)
-        locationAutocomplete.style["display"] = "block";
-        loc1text.textContent =  OSM.street + " " + OSM.house_number + " " + OSM.zip
-    }, 1000)
-    
-
-    
-}
-
-streetInput.addEventListener("onkeydown", ()  => {
-    alert("test")
-    locationAutocomplete.style["display"] = "block"
-});
+  OSMTimeoutID = window.setTimeout(async () => {
+    const OSM = await getOsmData(streetString)
+    locationAutocomplete?.classList.remove('hidden')
+    if (loc1text) loc1text.innerHTML = OSM[0].address.road
+    //loc1text.textContent = OSM.street + ' ' + OSM.house_number + ' ' + OSM.zip
+  }, 1000)
+})

d494f44004f72808a7dbb2beead5af34d0b82d7c
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-11-30 04:25:25 +0100

    feat: show empty edit dialog on add button press

--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -15,4 +15,34 @@ export default async function loadApp(user: string) {
       setCookie('user', '')
       window.location.reload()
     })
+
+  await loadDialogs()
+}
+
+const detailDialogHtmlURL = new URL(
+  './dialogs/detail/detail.html',
+  import.meta.url,
+)
+
+async function loadDialogs() {
+  const { EditDialog } = await import('./dialogs/edit')
+  const editDialog = new EditDialog()
+
+  // TODO: extract to separate class as well
+  await import('./dialogs/detail/detail.css')
+  const detailDialog = document.createElement('dialog')
+  const detailDialogHTML = await (await fetch(detailDialogHtmlURL)).text()
+  detailDialog.innerHTML = detailDialogHTML
+  detailDialog.id = 'detail-dialog'
+  document.body.appendChild(detailDialog)
+
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  document.querySelector('#add-btn')?.addEventListener('click', async () => {
+    await editDialog.load()
+    editDialog.show()
+
+    // TODO: load+store input data
+  })
+
+  // TODO: set up detail/edit dialog
 }

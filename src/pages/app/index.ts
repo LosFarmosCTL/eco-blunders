7db78e0316433a9fe447fbb4e60673365c5180c0
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-11-30 15:14:09 +0100

    refactor: move app JS to a class

--- src/pages/app/index.ts
+++ src/pages/app/index.ts
@@ -1,48 +1,54 @@
 import './app.css'
 import { setCookie } from '../../util/cookies'
+import { EditDialog } from './dialogs/edit'
 
 const appHtmlURL = new URL('./app.html', import.meta.url)
 
-export default async function loadApp(user: string) {
-  console.log(user)
+export class App {
+  private readonly user: string
 
-  const appHTML = await (await fetch(appHtmlURL)).text()
-  document.body.innerHTML = appHTML
+  constructor(user: string) {
+    this.user = user
+    console.log(user)
+  }
 
-  document
-    .querySelector<HTMLButtonElement>('#logout-btn')
-    ?.addEventListener('click', () => {
-      setCookie('user', '')
-      window.location.reload()
+  public async load() {
+    const appHTML = await (await fetch(appHtmlURL)).text()
+    document.body.innerHTML = appHTML
+
+    document
+      .querySelector<HTMLButtonElement>('#logout-btn')
+      ?.addEventListener('click', () => {
+        setCookie('user', '')
+        window.location.reload()
+      })
+
+    const { EditDialog } = await import('./dialogs/edit')
+    const editDialog = new EditDialog()
+
+    // TODO: extract to separate class as well
+    await import('./dialogs/detail/detail.css')
+    const detailDialog = document.createElement('dialog')
+    const detailDialogHTML = await (await fetch(detailDialogHtmlURL)).text()
+    detailDialog.innerHTML = detailDialogHTML
+    detailDialog.id = 'detail-dialog'
+    document.body.appendChild(detailDialog)
+
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    document.querySelector('#add-btn')?.addEventListener('click', async () => {
+      await addLocation(editDialog)
     })
 
-  await loadDialogs()
+    async function addLocation(dialog: EditDialog) {
+      await dialog.load(null)
+      await dialog.showModal()
+
+      // TODO: load+store input data
+    }
+  }
 }
 
 const detailDialogHtmlURL = new URL(
   './dialogs/detail/detail.html',
   import.meta.url,
 )
-
-async function loadDialogs() {
-  const { EditDialog } = await import('./dialogs/edit')
-  const editDialog = new EditDialog()
-
-  // TODO: extract to separate class as well
-  await import('./dialogs/detail/detail.css')
-  const detailDialog = document.createElement('dialog')
-  const detailDialogHTML = await (await fetch(detailDialogHtmlURL)).text()
-  detailDialog.innerHTML = detailDialogHTML
-  detailDialog.id = 'detail-dialog'
-  document.body.appendChild(detailDialog)
-
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  document.querySelector('#add-btn')?.addEventListener('click', async () => {
-    await editDialog.load(null)
-    await editDialog.show()
-
-    // TODO: load+store input data
-  })
-
-  // TODO: set up detail/edit dialog
-}

e9a8859992f8669e46961d8b34d88c9760113ac4
BunkeMonkey <135245949+BunkeMonkey@users.noreply.github.com>
2024-11-30 16:25:16 +0100

    feat: made lat and long inputs non interactive; set up new location class with interface and getting all input data; edited style of edit dialog when its adding a new location

--- src/pages/app/dialogs/edit/index.ts
+++ src/pages/app/dialogs/edit/index.ts
@@ -1,3 +1,4 @@
+import { Location } from './Location'
 export class EditDialog {
   private readonly editDialogHtmlURL = new URL('./edit.html', import.meta.url)
 
@@ -6,11 +7,19 @@ export class EditDialog {
   constructor() {
     this.dialog = document.createElement('dialog')
     this.dialog.id = 'add-dialog'
+    this.dialog.addEventListener('close', () => {
+      //TODO: closing via ESC is not preventable we can just direct it to do something
+      console.log('close detected')
+    })
   }
 
   public async load(location: Location | null = null) {
     await import('./edit.css')
     this.dialog.innerHTML = await (await fetch(this.editDialogHtmlURL)).text()
+
+    if (location === null) {
+      this.styleNewLocation()
+    }
 
     this.dialog
       .querySelector<HTMLInputElement>('input[name="name"]')
@@ -25,25 +34,37 @@ export class EditDialog {
   // TODO: return edited data
   public async showModal() {
     this.dialog.showModal()
+    this.dialog
+      .querySelector('#submit-location')
+      ?.addEventListener('click', (ev) => {
+        ev.preventDefault()
+        console.log('submit clicked')
+      })
+
+    this.dialog
+      .querySelector('#cancel-button')
+      ?.addEventListener('click', () => {
+        console.log('cancel clicked')
+        this.dialog.close()
+      })
 
     await import('./OSM/location_script')
+    //TODO: do a promise resolve on click event here to return
   }
-}
 
-interface Location {
-  name: string
-  description: string
-  city: string
-  postcode: string
-  street: string
-  lat: string
-  lon: string
-  tags: [
-    {
-      tag_text: string
-      color: string
-    },
-  ]
-  images: [ImageBitmap]
-  // TODO: first version probably still needs changes
+  private styleNewLocation() {
+    console.log('load called with null')
+    this.dialog
+      .querySelector<HTMLButtonElement>('#delete-button')
+      ?.classList.add('hidden')
+    const submitButton =
+      this.dialog.querySelector<HTMLButtonElement>('#submit-location')
+    if (submitButton) submitButton.innerHTML = 'Submit Location'
+
+    const buttondiv =
+      this.dialog.querySelector<HTMLDivElement>('#dialog-buttons')
+    if (buttondiv) {
+      buttondiv.style.justifyContent = 'right'
+    }
+  }
 }

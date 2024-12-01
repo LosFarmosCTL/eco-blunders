4c6d848479d3ceaa732a5ee9447c5fb143c8d525
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2024-12-01 19:53:16 +0100

    refactor: move Login to TSX

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,78 @@
+import { h, Fragment } from '../../util/jsx/pragma'
+
+import './login.css'
+
+export function Login(onLogin: (user: string) => void) {
+  let usernameInput: HTMLInputElement | null = null
+  let passwordInput: HTMLInputElement | null = null
+
+  function login() {
+    const user = usernameInput?.value
+    const pass = passwordInput?.value
+
+    if (user == 'admina' && pass == 'password') onLogin('admina')
+    else if (user == 'normalo' && pass == 'password') onLogin('normalo')
+    // TODO: build a nicer way to show an invalid login
+    else alert('Invalid login!')
+  }
+
+  return (
+    <>
+      <div id="login">
+        <div className="background-container flex flex-col justify-center gap-10">
+          <div className="login-container flex">
+            <div className="grow">
+              <h1 className="text-center">ecoBlunders Berlin</h1>
+            </div>
+            <div className="grow">
+              <form className="flex flex-col gap-10 w-full box-border">
+                <input
+                  type="text"
+                  name="username"
+                  autoComplete="username"
+                  placeholder="Username"
+                  required
+                  autoFocus
+                  ref={(el) => {
+                    usernameInput = el
+                  }}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                  ref={(el) => {
+                    passwordInput = el
+                  }}
+                />
+                <div className="flex justify-end">
+                  <button
+                    type="submit"
+                    className="btn-normal"
+                    onClick={(e) => {
+                      e.preventDefault()
+                      login()
+                    }}
+                  >
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <footer className="login-footer flex justify-end">
+            <ul className="flex gap-10 list-none">
+              <a href="">
+                <li>Privacy Policy</li>
+              </a>
+              <a href="">
+                <li>Imprint</li>
+              </a>
+            </ul>
+          </footer>
+        </div>
+      </div>
+    </>
+  )
+}

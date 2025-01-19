0080a62cbfa54212582f7f4534e1ba9cab62eb4d
BunkeMonkey <135245949+BunkeMonkey@users.noreply.github.com>
2025-01-19 03:46:31 +0100

    cleanup of login + sending correct http error now

--- src/server/routes/login.ts
+++ src/server/routes/login.ts
@@ -1,24 +1,29 @@
 import { Request, Response } from 'express'
 import { findOneUser } from '../mongoCRUDs'
 
+interface reqBody {
+  username: string
+  password: string
+}
+
 export async function loginPOST(req: Request, res: Response) {
   try {
     //let userDoc = await mongo_cruds.findOneUser("admina", "pass1234");
     console.log(req.body)
-    const userToLogin = req.body
+    const userToLogin = req.body as reqBody
 
     const users = await findOneUser(userToLogin.username, userToLogin.password)
     if (users) {
       console.log('result found, sending username')
       res.status(200).json({
-        username: users.username,
-        role: users.role,
+        username: users.username as string,
+        role: users.role as string,
       })
     } else {
-      res.status(404).send(`Users not found!`)
+      res.status(401).send(`Users or Password not found!`)
     }
   } catch (err) {
     console.log(err)
-    res.status(400).send('Something is not right!!')
+    res.status(500).send('Internal Server Error')
   }
 }

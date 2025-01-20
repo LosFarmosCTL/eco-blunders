ed175f5c13a8d735e4e04b3a3c7104782ca6aade
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-01-20 03:15:48 +0100

    refactor(API): clean up express routes

--- src/server/routes/loc.ts
+++ src/server/routes/loc.ts
@@ -1,32 +1,22 @@
 import { Request, Response } from 'express'
 import {
-  insertOneLocation,
-  findOneLocation,
-  deleteOneLocation,
-  findAllLocations,
-  updateOneLocation,
+  insertLocation,
+  getLocation,
+  deleteLocation,
+  getLocations,
+  updateLocation,
 } from '../mongoCRUDs'
 import { Location } from '../../shared/model/location'
 
-export async function locGET(req: Request, res: Response) {
-  console.log('get loc received')
-  const locations = await findAllLocations()
-  console.log('result found, sending locations')
+export async function locGET(_: Request, res: Response) {
+  const locations = await getLocations()
   res.status(200).json(locations)
 }
 
 export async function locGETOne(req: Request, res: Response) {
-  console.log('get one loc received')
-  const locid = req.params.id
-  console.log('locid: ', locid)
-  let location
-  try {
-    location = await findOneLocation(locid)
-  } catch (e) {
-    console.log(e)
-    res.status(500)
-    return
-  }
+  const locationId = req.params.id
+  const location = await getLocation(locationId)
+
   if (location) {
     res.status(200).json(location)
   } else {
@@ -35,39 +25,36 @@ export async function locGETOne(req: Request, res: Response) {
 }
 
 export async function locPOST(req: Request, res: Response) {
-  console.log(req.body)
   if (!validateLocation(req.body)) {
     res.status(400).send('Invalid location!')
     return
   }
-  const location = req.body
-  const result = await insertOneLocation(location)
-  console.log('inserting location')
+
+  const result = await insertLocation(req.body)
+
   res.status(201).json({
     id: result,
   })
 }
 
 export async function locPUT(req: Request, res: Response) {
-  console.log('updating one location')
   if (!validateLocation(req.body)) {
     res.status(400).send('Invalid location!')
     return
   }
-  const location = req.body
-  const result = await updateOneLocation(location)
+
+  const result = await updateLocation(req.body)
+
   if (result) {
-    res.status(204)
-    res.send()
+    res.status(204).send()
   } else {
     res.status(404).send(`Location not found!`)
   }
 }
 
 export async function locDELETE(req: Request, res: Response) {
-  const locid = req.params.id
-  console.log('deleting one location')
-  const result = await deleteOneLocation(locid)
+  const result = await deleteLocation(req.params.id)
+
   if (result) {
     res.status(204)
     res.send()

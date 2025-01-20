c742970f7545fd1df098f9d98d7ce394470702e7
LosFarmosCTL <80157503+LosFarmosCTL@users.noreply.github.com>
2025-01-20 03:15:48 +0100

    refactor(API): clean up express routes

--- src/server/routes/login.ts
+++ src/server/routes/login.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from 'express'
-import { findOneUser } from '../mongoCRUDs'
+import { getUser } from '../mongoCRUDs'
 
 interface reqBody {
   username: string
@@ -8,16 +8,13 @@ interface reqBody {
 
 export async function loginPOST(req: Request, res: Response) {
   try {
-    //let userDoc = await mongo_cruds.findOneUser("admina", "pass1234");
-    console.log(req.body)
     const userToLogin = req.body as reqBody
 
-    const users = await findOneUser(userToLogin.username, userToLogin.password)
+    const users = await getUser(userToLogin.username, userToLogin.password)
     if (users) {
-      console.log('result found, sending username')
       res.status(200).json({
-        username: users.username as string,
-        role: users.role as string,
+        username: users.login,
+        role: users.role,
       })
     } else {
       res.status(401).send(`Users or Password not found!`)
